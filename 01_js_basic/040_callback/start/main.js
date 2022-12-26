001befb0bb460de607e70153312b323ee5692ca7
terumotoriku <081kng@gmail.com>
2022-12-27 08:23:55 +0900

    コールバック関数

--- 01_js_basic/040_callback/start/main.js
+++ 01_js_basic/040_callback/start/main.js
@@ -1,15 +1,27 @@
-function fn(number) {
+function print(callback) {
+  const result = callback(2);
+  console.log(result);
+}
+
+function fn(number = 3) {
     return number * 2;
-  }
+}
+
+// debuggerと記述すると11行目でコードが止まる
+// debugger;
+
+// 関数を渡しているだけで実行はしていない
+// そのため丸括弧と引数は必要なし
+print(fn);
   
-  console.log(fn(2));
-  console.log(fn(4));
+  // console.log(fn(2));
+  // console.log(fn(4));
   
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
+  // const fnArrow = number => {
+  //   console.log(number);
+  //   return number * 2;
+  // };
   
-  const fnArrowObj = number => ({ result: number * 2 })
+  // const fnArrowObj = number => ({ result: number * 2 })
   
-  console.log(fnArrowObj(2));
+  // console.log(fnArrowObj(2));
