75fee989d2dbe09737df42f78fae05d2c55c8d2c
terumotoriku <081kng@gmail.com>
2022-12-26 22:48:39 +0900

    アロー関数

--- 01_js_basic/020_arrow_function/start/main.js
+++ 01_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,25 @@
 function fn(number) {
   return number * 2;
 }
+
+// 関数式、無名関数
+const fn2 = function (number) {
+  return number * 2;
+};
+
+console.log(fn2(2));
+
+// アロー関数 (省略形)
+const fnAroow = number => number * 2;
+
+// アロー関数 (省略なし)
+const fnAroow2 = number => {
+  console.log(number);
+  return number * 2;
+}
+
+// オブジェクトを返却する時にアロー関数を省略すると波括弧が関数のブロックと重複してしまい、文法的によろしくない
+// そのような場合は、丸括弧で囲う
+const fnArrowObj = number => ({ result: number * 2 });
+
+console.log(fnAroow(2));
