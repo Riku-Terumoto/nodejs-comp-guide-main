2975f0ea13e478784f3a2c167e9f4468d0c62028
terumotoriku <081kng@gmail.com>
2022-12-31 00:04:26 +0900

    三項演算子

--- 01_js_basic/070_conditional_operator/start/main.js
+++ 01_js_basic/070_conditional_operator/start/main.js
@@ -1,17 +1,19 @@
 // 三項演算子（ ? : ）
 
 const a = true;
-let resultA;
+// 三項演算子の条件は完全なるtrue,false以外だとtruthyな値かfalsyな値かでも決まる
+// let resultA = a ? 'true' : 'false';
+let resultA = a ? 10 : 10;
 
-if(a) {
-  resultA = "true";
+if (a) {
+  resultA = 'true';
 } else {
-  resultA = "false";
+  resultA = 'false';
 }
 console.log(resultA);
 
-// function getResult() {
-//   return a ? "true" : "false";
-// }
+function getResult() {
+  return a ? 'true' : 'false';
+}
 
 // console.log(getResult());
