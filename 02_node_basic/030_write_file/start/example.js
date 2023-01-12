099cd68e089ca21a6b12f66d5044b9d229c09dba
terumotoriku <081kng@gmail.com>
2023-01-12 22:26:07 +0900

    write_file

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,13 @@
-console.log('hello, node.js');
+// ファイルへの書き込み
+// __dirname, __filename
+
+const fs = require('fs');
+
+// __dirnameは現在実行したjsファイルが格納されているディレクトリまでのパス
+console.log(__dirname);
+// 実行中のファイルまでの絶対パス
+console.log(__filename);
+
+// writeFileSyncの第一引数はファイルを出力したい場所のパス
+fs.writeFileSync(__dirname + '/test.txt', 'hello, node.js');
+// console.log('hello, node.js');
