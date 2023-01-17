cf87c93489748c00dfdbbd13bdeb10f49ad1a6db
terumotoriku <081kng@gmail.com>
2023-01-18 08:07:33 +0900

    ES Module

--- 02_node_basic/050_module_system_esm/start/calc.js
+++ 02_node_basic/050_module_system_esm/start/calc.js
@@ -5,7 +5,7 @@ function minus(a, b) {
   return a - b;
 }
 
-module.exports = {
-  plus,
-  minus,
-};
+// ESmoduleを使用する際はpackage.jsonに"type" : "module"を追加する
+// そうすると、通常のexport,importがしようできる
+// もう一つの方法は拡張子をmjsに変更するとESmoduleが使用できるようになる
+export { plus, minus };
