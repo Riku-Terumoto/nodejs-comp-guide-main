4dc1ce878821bee985108200f33e018674b0d58e
terumotoriku <081kng@gmail.com>
2023-01-23 08:26:14 +0900

    is-odd

--- 03_npm/020_dep_vs_devDep/start/main.mjs
+++ 03_npm/020_dep_vs_devDep/start/main.mjs
@@ -1,5 +1,11 @@
-import isOdd from "is-odd";
+import isOdd from 'is-odd'; //奇数か偶数かを判定するパッケージ
 
 const result = isOdd(3);
 
 console.log(result);
+
+// package.jsonの中身
+// dependencies・・・依存という意味
+// その為dependencies内のis-oddがmain.mjsと依存関係であることを示している
+
+// devDependencies・・・開発環境でも本番環境でも依存の関係であるという意味
